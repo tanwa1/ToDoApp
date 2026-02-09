5db18c65ed7523daf98395664756d4a11aa003e3
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-02-10 00:04:05 +0800

    Able to do: Webpack Scaffold, toDo factory constructor/ classes, and basic UI, will do add / delete todos on one project later today

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export {default as addTask } from "./addTask.svg";
+export {default as complete } from "./complete.svg";
+export {default as noteApp } from "./noteApp.svg";
+export {default as pending } from "./pending.svg";
+export {default as upcoming } from "./upcoming.svg";
+export {default as urgent } from "./urgent.svg";
--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,4 @@
-import noteApp from "./assets/noteApp.svg";
+import { addTask, complete, noteApp, pending, upcoming, urgent } from "./assets/index.js";
 
 function render(project) {
     const appDiv = document.getElementById("app");
@@ -50,35 +50,86 @@ function renderNav() {
     projectsTitle.classList.add("projectTitle");
     projectsTitle.textContent = "PROJECTS";
 
-    const projectList = document.createElement("ul");
-    projectList.classList.add("projectList");
-    const projectItem = document.createElement("button");
-    projectItem.textContent = "Default Project";
-
-    projectsDiv.appendChild(projectList);
-    projectList.appendChild(projectItem);
     projectsDiv.appendChild(projectsTitle);
-    projectsDiv.appendChild(projectList);
+    
+    const projects = [
+        {
+            label: "Upcoming",
+            icon: upcoming,
+        },
+        {
+            label: "Pending",
+            icon: pending,
+        },
+
+        {
+            label: "Urgent",
+            icon: urgent,
+        },
+        {
+            label: "Complete",
+            icon: complete
+        }
+
+    ];
+    
+    projects.forEach(project => {
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("projectDivContainer")
+        const projectImage = document.createElement('img');
+        const projectButton = document.createElement("button");
+        projectButton.classList.add("projectLinks");
+        projectImage.src = project.icon;
+        projectButton.textContent = project.label;
+        projectDiv.appendChild(projectImage);
+        projectDiv.appendChild(projectButton);
+        projectsDiv.appendChild(projectDiv);
+    });
+
     projectsContainer.appendChild(projectsDiv);
     navDiv.appendChild(projectsContainer);
-    
+
     const buttonContainer = document.createElement("div");
     buttonContainer.classList.add("buttonContainer");
-    
+
     const addButton = document.createElement("button");
     addButton.classList.add("addButton");
     addButton.textContent = " + Add Project";
-    
+
 
     buttonContainer.appendChild(addButton);
     projectsContainer.appendChild(buttonContainer);
-    
+
     return navDiv;
 }
 
 function renderContent() {
     const contentDiv = document.createElement("div");
     contentDiv.classList.add("content");
+    
+    const headerDiv = document.createElement("div");
+    headerDiv.classList.add("headeDiv");
+    const header = document.createElement("h1");
+    header.textContent = "Template Projects"
+    
+    headerDiv.appendChild(header);
+    contentDiv.appendChild(headerDiv);
+    
+    const toDoDiv = document.createElement("div");
+    toDoDiv.classList.add("toDoDiv");
+    
+    const input = document.createElement("input");
+    input.setAttribute('type', 'text');
+    input.setAttribute('value','Add new todo...');
+    input.setAttribute('id', 'inputField');
+    
+    const addToDoButton = document.createElement("button");
+    addToDoButton.classList.add("addToDoButton");
+    addToDoButton.textContent = "Add To Do"
+    
+    toDoDiv.appendChild(input);
+    toDoDiv.appendChild(addToDoButton);
+    contentDiv.appendChild(toDoDiv);
     return contentDiv
 
 }

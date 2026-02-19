6a0b35f27998b351548a1b4aaa7589578da0d022
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2026-02-19 20:47:56 +0800

    Adding the new todo onto the selected project, but updating showing of UI will be tomorrow

--- src/controller.js
+++ src/controller.js
@@ -22,6 +22,8 @@ const projectLists = document.getElementById("addedProjectsContainer");
 
 const projects = [];
 
+let getProjectId = null;
+
 export function getId() {
     create.addEventListener('click', () => {
         let getProjectInput = document.getElementById("projectInput").value;
@@ -36,7 +38,6 @@ export function getId() {
         const projectModel = new Project(newProject.label, newProject.id);
 
         const projectContainer = document.createElement("div");
-        projectContainer.setAttribute('data-id', newProject.id);
         projectContainer.classList.add("projectDivContainer");
 
         const projectImage = document.createElement('img');
@@ -49,8 +50,8 @@ export function getId() {
         deleteImage.classList.add("deleteImage");
 
         const projectButton = document.createElement("button");
-        projectButton.classList.add('projectButton');
-
+        projectButton.setAttribute('data-id', newProject.id);
+        projectButton.className = 'projectButton';
 
         projectImage.src = newProject.icon1;
 
@@ -72,6 +73,7 @@ export function getId() {
 
 }
 
+
 addButton.addEventListener('click', () => {
     projectDialog.showModal();
 });
@@ -82,6 +84,12 @@ cancel.addEventListener('click', () => {
 
 
 projectLists.addEventListener('click', (event) => {
+
+    if (event.target.classList.contains('projectButton')) {
+        getProjectId = event.target.getAttribute('data-id');
+    }
+
+
     if (event.target.classList.contains('deleteImage')) {
         const deleteRow = event.target.closest(".projectDivContainer")
         const idRow = deleteRow.dataset.id;
@@ -95,42 +103,57 @@ projectLists.addEventListener('click', (event) => {
 
 todocreate.addEventListener('click', (event) => {
     event.preventDefault();
-    
+
     const todoListsDiv = document.querySelector('.todoListsDiv');
-    
+
     const toDoInput = document.getElementById("todoInput").value;
-    // const dueDateInput = document.getElementById("dueDate").value;
-    // const priorityInput = document.getElementById("priority").value;
+    const dueDateInput = document.getElementById("dueDate").value;
+    const priorityInput = document.getElementById("priority").value;
+    const description = document.getElementById("description").innerText;
 
+    const todoId = crypto.randomUUID();
 
-    const todoHeaderContainer = document.createElement("div");
-    todoHeaderContainer.className = 'toDoContainer';
+    const toDoContainer = document.createElement("div");
+    toDoContainer.className = 'toDoContainer';
+    toDoContainer.setAttribute('data-id', todoId)
 
-    const radioDiv = document.createElement("div");
-    radioDiv.className = 'radioDiv';
-    const radioDone = document.createElement("input");
-    radioDone.setAttribute('id', 'radioDone');
-    radioDone.setAttribute('type', 'checkbox');
-    radioDone.setAttribute('name', 'radioButton');
-    
-    const label = document.createElement('label');
-    label.setAttribute('for', 'radioDone')
-    
-    radioDiv.appendChild(radioDone);
-    radioDiv.appendChild(label);
-    
     const buttonColContainer = document.createElement("div");
     buttonColContainer.className = 'buttonColContainer';
-    
+
     const buttonCollapsible = document.createElement("button");
     buttonCollapsible.className = 'buttonCollapsible';
     buttonCollapsible.textContent = toDoInput;
-    
+
+
+    const todoName = document.createElement('div');
+    todoName.setAttribute('id', 'todoName');
+    todoName.textContent = toDoInput;
+
+
+    const middleRow = document.createElement("div");
+    middleRow.className = 'middleRow';
+
+    const descriptionPara = document.createElement("p");
+    descriptionPara.textContent = 'Description: ' + description;
+
+    const dueDatePara = document.createElement("p");
+    dueDatePara.textContent = 'Due Date: ' + dueDateInput;
+
+    const priorityPara = document.createElement("p");
+    priorityPara.textContent = 'Priority: ' + priorityInput;
+
+    const editButton = document.createElement('button');
+    editButton.className = 'editButton';
+    editButton.textContent = 'Edit Details';
+
     buttonColContainer.appendChild(buttonCollapsible);
-    
-    const buttonDeleteTodo = document.createElement("div");
-    buttonDeleteTodo.className = 'buttonDeleteTodo';
-    
+    buttonColContainer.appendChild(middleRow);
+
+    middleRow.appendChild(descriptionPara);
+    middleRow.appendChild(dueDatePara)
+    middleRow.appendChild(priorityPara)
+    middleRow.appendChild(editButton);
+
     const deleteTodo = document.createElement('button');
     deleteTodo.className = 'deleteTodo';
     const deleteTodoImg = document.createElement('img');
@@ -138,48 +161,62 @@ todocreate.addEventListener('click', (event) => {
     deleteTodoImg.src = deleteIcon;
 
     deleteTodo.appendChild(deleteTodoImg);
-    buttonDeleteTodo.appendChild(deleteTodo);
-    
-    todoHeaderContainer.appendChild(radioDiv);
-    todoHeaderContainer.appendChild(buttonColContainer);
-    todoHeaderContainer.appendChild(buttonDeleteTodo);
-    
-    
-    todoListsDiv.appendChild(todoHeaderContainer);
 
-    // var coll = document.getElementsByClassName("buttonCollapsible");
-    // var i;
+    const checkbox = document.createElement("div");
+    checkbox.className = 'radioDiv';
+    const checkBoxDone = document.createElement("input");
+    checkBoxDone.setAttribute('id', 'radioDone');
+    checkBoxDone.setAttribute('type', 'checkbox');
+    checkBoxDone.setAttribute('name', 'checkBoxDone');
 
-    // for (i = 0; i < coll.length; i++) {
-    //     coll[i].addEventListener("click", function () {
-    //         this.classList.toggle("active");
-    //         var content = this.nextElementSibling;
-    //         if (content.style.maxHeight) {
-    //             content.style.maxHeight = null;
-    //         } else {
-    //             content.style.maxHeight = content.scrollHeight + "px";
-    //         }
-    //     });
-    // }
 
-    // const middleRow = document.createElement("div");
-    // middleRow.className = 'middleRow';
+    const todos = new ToDo(toDoInput, description, dueDateInput, priorityInput, checkBoxDone.checked, todoId);
 
-    // const dueDatePara = document.createElement("p");
-    // dueDatePara.textContent = dueDateInput;
+    const matchedProject = projects.find(project => project.id === getProjectId);
 
-    // const priorityPara = document.createElement("p");
-    // priorityPara.textContent = priorityInput;
+    if (matchedProject) {
+        matchedProject.todos.push(todos);
+    }
 
-    // middleRow.appendChild(dueDatePara)
-    // middleRow.appendChild(priorityPara)
+    console.log(todos);
 
-    // todoHeaderContainer.appendChild(middleRow)
-    // todoDialog.close();
+
+    checkBoxDone.addEventListener('change', () => {
+        todos.completed = checkBoxDone.checked;
+        console.log(todos);
+    });
+
+    const label = document.createElement('label');
+    label.setAttribute('for', 'radioDone')
+
+    checkbox.appendChild(checkBoxDone);
+    checkbox.appendChild(label);
+
+    toDoContainer.appendChild(buttonColContainer);
+    toDoContainer.appendChild(checkbox);
+    toDoContainer.appendChild(deleteTodo);
+
+
+    todoListsDiv.appendChild(toDoContainer);
+    todoListsDiv.appendChild(middleRow);
+
+    buttonCollapsible.addEventListener("click", function () {
+        this.classList.toggle("active");
+        if (middleRow.style.maxHeight) {
+            middleRow.style.maxHeight = null;
+        } else {
+            middleRow.style.maxHeight = middleRow.scrollHeight + "px";
+        }
+    });
+
+    middleRow.style.maxHeight = null;
+
+    todoDialog.close();
 
     return todoListsDiv;
 
 });
+
 
 addToDoButton.addEventListener('click', () => {
     todoDialog.showModal();
